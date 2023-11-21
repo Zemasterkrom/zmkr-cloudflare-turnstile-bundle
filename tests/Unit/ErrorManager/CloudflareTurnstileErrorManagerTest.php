b83d79498e218c0afac7c80bf0ed76853902db58
Zemasterkrom <raphael.kimm@outlook.fr>
2023-11-21 19:26:20 +0100

    Added tests for ErrorManager

---
+++ tests/Unit/ErrorManager/CloudflareTurnstileErrorManagerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Zemasterkrom\CloudflareTurnstileBundle\Tests\ErrorManager;
+
+use PHPUnit\Framework\TestCase;
+use Zemasterkrom\CloudflareTurnstileBundle\ErrorManager\CloudflareTurnstileErrorManager;
+use Zemasterkrom\CloudflareTurnstileBundle\Exception\CloudflareTurnstileException;
+
+/**
+ * Cloudflare Turnstile exception / error manager test class.
+ * Checks that exception throwing is properly enabled / disabled / configured.
+ */
+class CloudflareTurnstileErrorManagerTest extends TestCase
+{
+    public function testExceptionThrowingWithEnabledCoreFailureHandling(): void
+    {
+        $this->expectException(CloudflareTurnstileException::class);
+
+        $errorManager = new CloudflareTurnstileErrorManager(true);
+        $errorManager->throwIfExplicitErrorsEnabled(new CloudflareTurnstileException(''));
+    }
+
+    public function testNoExceptionThrowingWithDisabledCoreFailureHandling(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $errorManager = new CloudflareTurnstileErrorManager(false);
+        $errorManager->throwIfExplicitErrorsEnabled(new CloudflareTurnstileException(''));
+    }
+}
