7875411636e165478270d5bab278368dc38c7b6e
Zemasterkrom <raphael.kimm@outlook.fr>
2024-01-02 15:20:21 +0100

    Language auto-configuration

--- src/Form/Type/CloudflareTurnstileType.php
+++ src/Form/Type/CloudflareTurnstileType.php
@@ -3,11 +3,11 @@
 namespace Zemasterkrom\CloudflareTurnstileBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Exception\InvalidConfigurationException;
+use Symfony\Component\Form\Exception\LogicException;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -50,10 +50,12 @@ class CloudflareTurnstileType extends AbstractType
     ];
 
     private CloudflareTurnstilePropertiesManager $propertiesManager;
+    private ?RequestStack $requestStack;
 
-    public function __construct(CloudflareTurnstilePropertiesManager $propertiesManager)
+    public function __construct(CloudflareTurnstilePropertiesManager $propertiesManager, RequestStack $requestStack = null)
     {
         $this->propertiesManager = $propertiesManager;
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -64,48 +66,60 @@ class CloudflareTurnstileType extends AbstractType
         $resolver->setDefaults([
             'individual_explicit_js_loader' => null,
             'mapped' => false,
+            'attr' => [
+                'data-sitekey' => $this->propertiesManager->getSitekey(),
+                'class' => 'cf-turnstile'
+            ],
             'constraints' => [
                 new CloudflareTurnstileCaptcha()
             ]
         ]);
 
         $resolver->setNormalizer('attr', function (Options $options, array $attributes) {
+            $normalizedLocale = null;
+
+            if (isset($attributes['data-response-field-name'])) {
+                throw new LogicException('Unable to change the data-response-field-name attribute as it is automatically generated for validation');
+            }
+
             if (isset($attributes['data-language'])) {
                 if (!\is_string($attributes['data-language']) && !(\is_object($attributes['data-language']) && method_exists($attributes['data-language'], '__toString'))) {
-                    throw new InvalidOptionsException('The Cloudflare Turnstile captcha language must be represented by a supported language code');
+                    throw new LogicException('The Cloudflare Turnstile captcha language must be represented by a supported language code');
                 }
 
-                $attributes['data-language'] = str_replace('_', '-', strtolower((string) $attributes['data-language']));
+                $normalizedLocale = str_replace('_', '-', strtolower((string) $attributes['data-language']));
+
+                if (!isset(self::SUPPORTED_LANGUAGES_LOCALES[$normalizedLocale])) {
+                    throw new InvalidOptionsException(sprintf('The %s locale is not supported by Cloudflare Turnstile or the current bundle version.', $normalizedLocale));
+                }
+            } else {
+                $request = $this->requestStack ? $this->requestStack->getCurrentRequest() : null;
+                $normalizedLocale = $request ? str_replace('_', '-', strtolower($request->getLocale())) : null;
+
+                if ($normalizedLocale && !isset(self::SUPPORTED_LANGUAGES_LOCALES[$normalizedLocale])) {
+                    @trigger_error(sprintf('The %s locale is not supported by Cloudflare Turnstile or the current bundle version. Switching back to default browser language detection for the Cloudflare Turnstile captcha.', $normalizedLocale), E_USER_NOTICE);
+                    $normalizedLocale = null;
+                }
+            }
+
+            if ($normalizedLocale) {
+                $attributes['data-language'] = $normalizedLocale;
             }
 
             if (isset($attributes['class']) && !\is_scalar($attributes['class']) && !(\is_object($attributes['class']) && method_exists($attributes['class'], '__toString'))) {
-                throw new InvalidOptionsException('Cloudflare Turnstile captcha widget class must be stringable');
+                throw new LogicException('Cloudflare Turnstile captcha widget class must be stringable');
             }
 
             $attributes['class'] = isset($attributes['class']) && (string) $attributes['class'] && !\is_bool($attributes['class']) ? (preg_match("/\bcf-turnstile\b/", $attributes['class']) ? $attributes['class'] : 'cf-turnstile ' . $attributes['class']) : 'cf-turnstile';
 
             return $attributes;
         });
-    }
 
-    /**
-     * Builds the form.
-     *
-     * Cloudflare Turnstile captcha options are checked for consistency during form building.
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        if (!isset($options['constraints'][0]) || !$options['constraints'][0] instanceof CloudflareTurnstileCaptcha) {
-            throw new InvalidConfigurationException('The first constraint of the Cloudflare Turnstile captcha must be an instance of a Cloudflare Turnstile captcha constraint');
-        }
-
-        if (isset($options['attr']['data-language']) && !isset(self::SUPPORTED_LANGUAGES_LOCALES[$options['attr']['data-language']])) {
-            throw new InvalidOptionsException(sprintf('The %s locale is not supported by Cloudflare Turnstile or current bundle version', $options['attr']['data-language']));
-        }
-
-        if (isset($options['individual_explicit_js_loader']) && !\is_scalar($options['individual_explicit_js_loader']) && !(\is_object($options['individual_explicit_js_loader']) && method_exists($options['individual_explicit_js_loader'], '__toString'))) {
-            throw new InvalidOptionsException('Cloudflare Turnstile captcha individual widget explicit JavaScript loader must be stringable');
-        }
+        $resolver->setNormalizer('individual_explicit_js_loader', function (Options $options, $individualExplicitJsLoader) {
+            if (isset($individualExplicitJsLoader) && !\is_scalar($individualExplicitJsLoader) && !(\is_object($individualExplicitJsLoader) && method_exists($individualExplicitJsLoader, '__toString'))) {
+                throw new LogicException('Cloudflare Turnstile captcha individual widget explicit JavaScript loader must be stringable');
+            }
+        });
     }
 
     /**
@@ -117,7 +131,6 @@ class CloudflareTurnstileType extends AbstractType
         $view->vars['individual_explicit_js_loader'] = $options['individual_explicit_js_loader'];
         $view->vars['compatibility_mode'] = &$this->propertiesManager->getCompatibilityMode();
         $view->vars['enabled'] = $this->propertiesManager->isEnabled();
-        $view->vars['attr']['data-sitekey'] = $this->propertiesManager->getSitekey();
         $view->vars['attr']['data-response-field-name'] = $view->vars['full_name'];
     }
 
